7b3c5bd3708bebdaaf44594d92e2517f97e056f5
Franco Arce <89941404+arcefranco@users.noreply.github.com>
2023-04-11 13:04:32 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <div>
       <Nav />
       <Routes>
-        <h1>hola</h1>
+        <h1>hola1</h1>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
